7c3272eb69bb85eaf8550008d6c9787050e09b9c
Ktari ayman <aymanktari31@gmail.com>
2023-02-03 13:58:10 +0100

    dummy data

---
+++ frontend/src/data.ts
@@ -0,0 +1,32 @@
+import { Person } from "./types/Person";
+
+export const users: Person[] = [
+  {
+    id: 1,
+    firstName: "Ayman",
+    lastName: "Ktari",
+    email: "aymanktari31@gmail.com",
+    image: "../public/images/p1.jpg",
+  },
+  {
+    id: 2,
+    firstName: "wassim",
+    lastName: "Ktari",
+    email: "wassimktari99@gmail.com",
+    image: "../public/images/p2.jpg",
+  },
+  {
+    id: 3,
+    firstName: "Ayman",
+    lastName: "Ktari",
+    email: "aymanktari31@gmail.com",
+    image: "../public/images/p1.jpg",
+  },
+  {
+    id: 4,
+    firstName: "wassim",
+    lastName: "Ktari",
+    email: "wassimktari99@gmail.com",
+    image: "../public/images/p2.jpg",
+  },
+];
